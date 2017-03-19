1e23ddb13b12636217c48affc8d3f7804fd53887
Brijesh Prasad <brijeshp@Brijeshs-MacBook-Pro.local>
2017-03-19 23:12:17 +0530

    Added service for Add and Delete post

--- public/js/services/todos.js
+++ public/js/services/todos.js
@@ -7,6 +7,20 @@ angular.module('todoService', [])
                 }
             }
         }])
+        .factory('AddPostService', ['$http', function($http){
+          return {
+            save : function(post){
+              return $http.post('http://jsonplaceholder.typicode.com/posts',post);
+            }
+          }
+        }])
+        .factory('DeletePostService', ['$http', function($http){
+          return {
+            delete : function(id){
+              return $http.delete('http://jsonplaceholder.typicode.com/posts/'+id);
+            }
+          }
+        }])
     .filter('interpolate', ['version', function (version) {
         return function (text) {
             return String(text).replace(/\%VERSION\%/mg, version);
