eaeb7a64bef0affd77b350e5b3c70bb23bd2e6e7
Brijesh Prasad <brijeshp@Brijeshs-MacBook-Pro.local>
2017-03-19 23:12:53 +0530

    Add and delete post

--- public/module1/DashboardCtrl.js
+++ public/module1/DashboardCtrl.js
@@ -1,5 +1,5 @@
 angular.module('DashboardCtrl', [])
-.controller('DashboardController', ['$scope', '$http','filterFilter', '$filter', 'ngTableParams','GetPosts', function ($scope, $http, filterFilter, $filter, ngTableParams,GetPosts) {
+.controller('DashboardController', ['$scope', '$http','filterFilter', '$filter', 'ngTableParams','GetPosts','AddPostService','DeletePostService', function ($scope, $http, filterFilter, $filter, ngTableParams,GetPosts,AddPostService,DeletePostService) {
 
 $scope.getPosts = function(){
     GetPosts.get()
@@ -11,5 +11,22 @@ $scope.getPosts = function(){
     });
 }
 $scope.getPosts();
+
+$scope.savePost =  function(){
+  var Promise = AddPostService.save($scope.post);
+  Promise.then(function(response){
+    var post =  response.data;
+    alert("Following post has been created\n"+"Id: "+post.id+"\nUser-id: "+post.userid+"\nTitle: "+post.title+"\nBody:"+post.body);
+  });
+  $scope.post = null;
+  angular.element('#myModal').modal('hide');
+}
+
+$scope.deletePost =  function(id){
+  var deletePostPromise = DeletePostService.delete(id);
+  deletePostPromise.then(function(response){
+    alert("Post with ID "+id+" deleted successfully");
+  })
+}
 
 }]);
